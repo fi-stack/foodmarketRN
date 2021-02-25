211da2c66afb3af0cd7675bb77d7b7bbe2746d85
rafi-windows <rafi_n_o@github.com>
2021-02-25 10:13:25 +0700

    move between page with stack navigation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import 'react-native-gesture-handler';
 import {SafeAreaView, Text, StatusBar} from 'react-native';
 import {SplashScreen, SignIn} from './pages';
 import {NavigationContainer} from '@react-navigation/native';
+import Router from './router';
 
 const App = () => {
   return (
     <NavigationContainer>
-      {/* <SplashScreen /> */}
-      <SignIn />
+      <Router />
     </NavigationContainer>
   );
 };
